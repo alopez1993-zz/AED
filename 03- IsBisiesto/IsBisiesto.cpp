e59a856ed5287cb8fd2f7590c0fe5be2d73769a6
Axel J. López <63621306+alopez1993@users.noreply.github.com>
2020-06-09 00:04:00 -0300

    Cambio de tipo de dato de "year" a int

--- 03- IsBisiesto/IsBisiesto.cpp
+++ 03- IsBisiesto/IsBisiesto.cpp
@@ -36,10 +36,11 @@ int main ()
     
     return 0;
 }
-// m: year > 1582; p: year%4 == 0; q: year%100 == 0; r: year%400 == 0;
-bool IsBisiesto (unsigned);
 
-bool IsBisiesto (unsigned year)
+// m: year > 1582; p: year%4 == 0; q: year%100 == 0; r: year%400 == 0;
+bool IsBisiesto (int);
+
+bool IsBisiesto (int year)
     {
         return year > 1582 and (year%4 == 0) and ((year%100 != 0) or year%400 == 0); // m ^ (p ^ (-q v r))
     }
