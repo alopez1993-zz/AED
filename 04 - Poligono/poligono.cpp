6dbce629827530f9ddb3a5001224e33cf284d4b8
Axel J. López <63621306+alopez1993@users.noreply.github.com>
2020-09-01 23:04:05 -0300

    Borrador 1

--- 04 - Poligono/poligono.cpp
+++ 04 - Poligono/poligono.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <cassert>
+#include <array>
 
 using namespace std;
+int main () 
+{
+    struct punto {double x, y;};
+    struct triangulo {array<punto, 3> puntostri; string colortri};
+    struct poligono { unsigned p; array<punto, 50> puntospol; string colorpol;};
 
-struct punto {int x; int y}
-struct poligono { unsigned p; array <punto, 30>}
-using triangulo = array <punto, 3>}
+    void SetPuntoTri (triangulo&, punto);
+    void SetPuntoPol (poligono&, punto, unsigned);
+    void AgregarPuntoFinal (poligono&, punto);
+    void CambiarColorPol (poligono&, string);
+    void CambiarColorTri (triangulo&, string);
+    punto QuitarPuntoTri (triangulo&);
+    punto QuitarPuntoPol (poligono&);
+    // PuntoEliminado = QuitarPuntoPol (pentágono1)
+    color GetColorPol (const poligono&)
+    // ColorPoligono = GetColorPol (pentágono1)
+
+    
+}
