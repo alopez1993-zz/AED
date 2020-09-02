b181af3167b1f02893e3ce8be35e21b11bdf8145
Axel J. López <63621306+alopez1993@users.noreply.github.com>
2020-09-02 00:03:59 -0300

    lados y angulos

--- 04 - Poligono/poligono.cpp
+++ 04 - Poligono/poligono.cpp
@@ -10,18 +10,22 @@ int main ()
     struct triangulo {array<punto, 3> puntostri; string colortri;};
     struct poligono { unsigned p; array<punto, 50> puntospol; string colorpol;};
 
-    void SetPuntoTri (triangulo&, punto);
+    void SetPuntoTri (triangulo&, punto, unsigned); //el unsigned será la posición del punto a cambiar
     void SetPuntoPol (poligono&, punto, unsigned); //el unsigned será la posición del punto a cambiar
     void AgregarPuntoFinal (poligono&, punto);
     void CambiarColorPol (poligono&, string); //el string será el color nuevo
     void CambiarColorTri (triangulo&, string); //el string será el color nuevo
-    punto QuitarPuntoTri (triangulo&);
     punto QuitarPuntoPol (poligono&);
     // PuntoEliminado = QuitarPuntoPol (pentágono1)
     string GetColorTri (const triangulo&);
     string GetColorPol (const poligono&);
     // ColorPoligono = GetColorPol (pentágono1)
-
+    string GetPuntoPol (const poligono&, unsigned); //el unsigned será la posición del punto a pedir
+    string GetPuntoTri (const triangulo&, unsigned); //el unsigned será la posición del punto a pedir
+    double LongitudLadoPol (const poligono&, unsigned, unsigned); // los unsigned serán la posición de dos puntos adyacentes
+    double LongitudLadoTri (const poligono&, unsigned, unsigned); // los unsigned serán la posición de dos puntos adyacentes
+    using angulos = array<unsigned,3>;
+    angulos angulostri (const triangulo&); //esta función convocaría a LongitudLadoTri 3 veces, una por cada lado, y a una función coseno. Teorema del coseno.
     system("PAUSE()");
     return 0;
 }
