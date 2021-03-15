4aa2444fe6c6c064cb0ffdc12ccc7f0797243ab9
Axel J. López <63621306+alopez1993@users.noreply.github.com>
2021-03-15 18:21:47 -0300

    Cambios Finales 3

--- TrabajoFinal/TrabajoFinal.h
+++ TrabajoFinal/TrabajoFinal.h
@@ -47,33 +47,45 @@ struct ListaPol{
 };
 
 void FlujoPoligonos ();
+
+//Operaciones
 void AddFirstVertice (Poligono& pol, const Punto& punto);
 void AddFinalVertice (Poligono& pol, const Punto& punto);
+void AddPoligono (const Poligono& pol, ListaPol& listapol);
+float GetPerimetro (const Poligono& pol);
+float GetDistancia (const Punto& p1, const Punto& p2);
+
+//Salida standard
 void MostrarPoligono (const Poligono& pol);
 void MostrarPunto (const Node* nodo);
 void MostrarColor (const Poligono& pol);
 void MostrarPuntos (const Poligono& pol);
+void MostrarPerimetro (const Poligono& pol);
+void MostrarListaPol (const ListaPol& listapol);
+
+//Parse
 bool ParsearPoligonos (ifstream& in, ListaPol& listapol);
 bool ParsearPoligono (ifstream& in, Poligono& pol);
 bool ParsearColor (ifstream& in, Poligono& pol);
 bool ParsearPuntos (ifstream& in, Poligono& pol);
 bool ParsearPunto (ifstream& in, Punto& punto);
 bool ParsearPuntoComa (ifstream& in);
-void AddPoligono (const Poligono& pol, ListaPol& listapol);
-void MostrarListaPol (const ListaPol& listapol);
-float GetPerimetro (const Poligono& pol);
-void FiltrarPoligonos (ListaPol& listapol, const int& cond);
-bool CumpleCondicionPerimetro (const Poligono& pol, const int& cond);
-void DesenlazarNodoPol(ListaPol& listapol, NodePol* nodePol);
+
+//Enviar
 void EnviarPoligonosFiltrados (ListaPol& listapol, const int& cond, ofstream& out);
 void EnviarPoligonos (ListaPol& listapol, ofstream& out);
 void EnviarPoligono (Poligono& pol, ofstream& out);
 void EnviarColor (Color& color, ofstream& out);
 void EnviarPuntos (Node* nodo, ofstream& out);
-float GetDistancia (const Punto& p1, const Punto& p2);
 void EnviarPerimetro (const Poligono& pol, ofstream& out);
+
+//Filtros
+void FiltrarPoligonos (ListaPol& listapol, const int& cond);
+bool CumpleCondicionPerimetro (const Poligono& pol, const int& cond);
+void DesenlazarNodoPol(ListaPol& listapol, NodePol* nodePol);
+
+//Heap
 void LiberarPoligono (NodePol*& nodePol);
-void MostrarPerimetro (const Poligono& pol);
 
 
 
